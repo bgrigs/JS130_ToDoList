329ac20b17c9e7f61d2beebdb8bdaa09db5e0875
Bethany Grigsby <grigsby.bethany@gmail.com>
2024-10-11 13:29:27 -0700

    added jest testing

--- todolist.js
+++ todolist.js
@@ -2,36 +2,7 @@
 // data: the todo title and a flag that shows whether the
 // todo item is done.
 
-class Todo {
-  static DONE_MARKER = "X";
-  static UNDONE_MARKER = " ";
-
-  constructor(title) {
-    this.title = title;
-    this.done = false;
-  }
-
-  toString() {
-    let marker = this.isDone() ? Todo.DONE_MARKER : Todo.UNDONE_MARKER;
-    return `[${marker}] ${this.title}`;
-  }
-
-  markDone() {
-    this.done = true;
-  }
-
-  markUndone() {
-    this.done = false;
-  }
-
-  isDone() {
-    return this.done;
-  }
-
-  getTitle() {
-    return this.title;
-  }
-}
+const Todo = require(`./todo.js`);
 
 class TodoList {
   constructor(title) {
@@ -144,20 +115,4 @@ class TodoList {
   }
 }
 
-let list = new TodoList(`Today's todos`);
-
-let todo1 = new Todo("Buy milk");
-let todo2 = new Todo("Clean room");
-let todo3 = new Todo("Go to the gym");
-let todo4 = new Todo("Go shopping");
-let todo5 = new Todo("Feed the cats");
-let todo6 = new Todo("Study for Launch School");
-
-list.add(todo1);
-list.add(todo2);
-list.add(todo3);
-list.add(todo4);
-list.add(todo5);
-list.add(todo6);
-
-console.log(`${list}`);
+module.exports = TodoList;
